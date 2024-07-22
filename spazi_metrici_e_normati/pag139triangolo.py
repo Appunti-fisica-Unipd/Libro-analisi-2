1d06d4a752f5be365e4e68ed1c29e3500c270dd8
Emanuele Urso <ursomanuele@gmail.com>
2024-07-22 16:36:06 +0200

    fino a pag 140

--- spazi_metrici_e_normati/pag139triangolo.py
+++ spazi_metrici_e_normati/pag139triangolo.py
@@ -8,7 +8,7 @@ mpl.rcParams.update({'font.size': 20}) #font size
 fig, ax1 = plt.subplots()
 
 # Select length of axes and the space between tick labels
-xmin, xmax, ymin, ymax = -.1, 1.5, -.5, .5
+xmin, xmax, ymin, ymax = -.1, 2.1, -.1, 2.1
 
 # Set identical scales for both axes
 ax1.set(xlim=(xmin, xmax), ylim=(ymin, ymax), aspect=1)
@@ -30,34 +30,29 @@ arrow_fmt = dict(markersize=4, color='black', clip_on=False)
 ax1.plot((1), (0), marker='>', transform=ax1.get_yaxis_transform(), **arrow_fmt)
 ax1.plot((0), (1), marker='^', transform=ax1.get_xaxis_transform(), **arrow_fmt)
 
-# Create functions
-x = np.linspace(0, 1)
-
-def bfun(x):
-    return np.array(0 * x) #riclicato il vecchio programma
-
-def rfun(x):
-    return np.array( (x - .1) * (x - .6) * (x - 1.1) * 2)
-
 
 # Plot lines
-ax1.plot(x, bfun(x), color="b")
-ax1.plot(x, bfun(x) - .2, color="y")
-ax1.plot(x, bfun(x) + .2, color="g")
-ax1.plot(x, rfun(x), color="r")
+ax1.plot([0, 1/8], [0,2], color="r")
+ax1.plot([1/8, 1/4], [2,0], color="r")
+ax1.plot([1/4, 1], [0, 0], color="r")
+ax1.plot([0, 1/8], [2, 2], color="r", ls="--")
 
 
 # Use tex in labels
-ax1.set_xticks([1])
-ax1.set_xticklabels([r"$1$"])
-ax1.set_yticks([])
+ax1.set_xticks([1/4, 1])
+ax1.set_xticklabels([r"$\frac{1}{2^n}$", r"$1$"])
+ax1.set_yticks([2])
+ax1.set_yticklabels([r"$n$"])
+
+# Fill area
+ax1.fill_between(x=[0, 1/8-.005], y1=[0, 2], color='c', alpha=.2)
+ax1.fill_between(x=[1/8, 1/4], y1=[2, 0], color='c', alpha=.2)
+
+
 
 
 #Add text
-ax1.text(1.1, bfun(1), r"$y=f(x) = 0$", color="b")
-ax1.text(1.1, bfun(1) + .2, r"$y=f(x)+r$ = r", color="g")
-ax1.text(1.1, bfun(1) - .2, r"$y=f(x)-r$ = -r", color="y")
-ax1.text(1.1, rfun(1), r"$y=g(x)$", color="r")
+ax1.text(1/4, 1, r"$y=g_n(x)$", color="r")
 
 
 
